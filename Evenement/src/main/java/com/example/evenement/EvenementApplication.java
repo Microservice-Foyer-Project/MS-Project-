d163bd9749175ed3aff855801f92d98743946d49
unknown <feryel.ouerfelli@esprit.tn>
2023-12-14 11:56:51 +0100

    Eurika

--- Evenement/src/main/java/com/example/evenement/EvenementApplication.java
+++ Evenement/src/main/java/com/example/evenement/EvenementApplication.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EvenementApplication {
 
     public static void main(String[] args) {
